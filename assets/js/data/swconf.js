1a245db0f87e92e43baf5898802bf99e8de27c06
yezanee <yezanee@users.noreply.github.com>
2025-07-22 08:41:24 +0000

    deploy: dc973d33e8d1bc6a51ed68452db617e19f97dbb5

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753169881',resources: [
+    cacheName: 'chirpy-1753173683',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
